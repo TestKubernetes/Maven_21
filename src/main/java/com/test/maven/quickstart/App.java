948d3d02607937f7c5a8d585b14916592b06760f
aparna <aparnaondwar@gmail.com>
2020-09-22 14:38:45 +0530

    first

---
+++ src/main/java/com/test/maven/quickstart/App.java
@@ -0,0 +1,15 @@
+package com.test.maven.quickstart;
+
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+       System.out.println( "Hello World!" );
+    	
+    }
+}
